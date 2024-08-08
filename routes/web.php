385b1998f611bc6790249fd96da919c7e909eefe
yssakurabo <ys.sakurabo@gmail.com>
2024-08-08 16:48:17 +0900

    セクション4 項49.エラー

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
-
+use App\Http\Controllers\User\Auth\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,17 +17,21 @@ use App\Http\Controllers\LifeCycleTestController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user.welcome');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+    return view('user.dashboard');
+})->middleware(['auth.users'])->name('dashboard');
 
 Route::get('/component-test1', [ComponentTestController::class, 'showComponent1']);
 Route::get('/component-test2', [ComponentTestController::class, 'showComponent2']);
 Route::get('/servicecontainertest', [LifeCycleTestController::class, 'showServiceContainerTest']);
 Route::get('/serviceprovidertest', [LifeCycleTestController::class, 'showServiceProviderTest']);
 
+// AuthenticatedSessionController のルートを追加
+Route::get('/user/login', [AuthenticatedSessionController::class, 'create'])->name('user.login');
+Route::post('/user/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/user/logout', [AuthenticatedSessionController::class, 'destroy'])->name('user.logout');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
