bed04d98554ae92ca5376a3f9680b20ffe73f123
yssakurabo <ys.sakurabo@gmail.com>
2024-08-08 11:50:30 +0900

    変数

--- app/Http/Controllers/ComponentTestController.php
+++ app/Http/Controllers/ComponentTestController.php
@@ -9,7 +9,10 @@ class ComponentTestController extends Controller
     //
     public function showComponent1()
     {
-        return view('tests.component-test1');
+        $message = 'メッセージ123';
+        return view('tests.component-test1',
+        compact('message')
+    );
     }
     public function showComponent2()
     {
