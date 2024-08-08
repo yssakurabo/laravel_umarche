ee4e2708200508d31fe5a3906d217bc9e80b3285
yssakurabo <ys.sakurabo@gmail.com>
2024-08-08 13:33:41 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネントです
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+</div>
